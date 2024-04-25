ef809abdca2a3a86c536bc414e457eb7ef22c658
SalvatoreFerrara <167888917+LMP-FINANCE@users.noreply.github.com>
2024-04-25 10:52:02 +0200

    fix

--- script/home.js
+++ script/home.js
@@ -1,5 +1,5 @@
 // Verifica se la pagina è stata caricata con un errore 404
 if (document.querySelector('body').innerText.includes('404') || document.title === '404 Not Found') {
   // Reindirizza alla tua pagina di errore personalizzata
-  window.location.href = '/index.html';
+  window.location.href = '/a.html';
 }
